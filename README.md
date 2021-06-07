ac6b39ece2ac544ded8fe4ede32e4d2c77b14f84
RoNiNxXxsAs <erikcollegecc@gmail.com>
2021-06-07 18:09:53 -0400

    desafio

--- README.md
+++ README.md
@@ -3,5 +3,3 @@ Apos download, faça as configure as conexões do banco de dados.
 Apos configuração do BD rode os seguintes comandos:
 
 php artisan migrate
-php artisan storage:link
--------
--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -4,10 +4,11 @@
 
 @section('content')
 
-    <h1>Cadatrar Novo Post</h1>
-
-    <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data">
-        @include('admin.post._partials.form')
-    </form>
+    <h1 class="text-center text-3x1 uppercase font-black my-4">Cadatrar Novo Post</h1>
+    <div class="w-11/12 bg-white sm:w-8/12 md:w 1/2 mx-auto">
+        <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data">
+            @include('admin.post._partials.form')
+        </form>
+    </div>
 
 @endsection
